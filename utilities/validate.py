71d3e8e6990f8bf31caad970367ad9b3bc3b8b1d
Mark Baggett <mbagget1@utk.edu>
2022-11-09 09:14:05 -0500

    Find all errors up front.

--- utilities/validate.py
+++ utilities/validate.py
@@ -9,7 +9,9 @@ class AdditionalChecks:
         self.path = m3
         self.m3 = yaml.safe_load(open(m3))
         self.unique_classes = self.find_unique_classes()
+        self.all_exceptions = []
         self.validate_available_on()
+        self.raise_exceptions()
 
     def find_unique_classes(self):
         return [work_type for work_type in self.m3['classes']]
@@ -18,7 +20,14 @@ class AdditionalChecks:
         for property in self.m3['properties']:
             for work_type in self.m3['properties'][property]['available_on']['class']:
                 if work_type not in self.unique_classes:
-                    raise ValueError(f'Unknown worktype {work_type} in {property} in {self.path}.')
+                    self.all_exceptions.append(f'Unknown worktype {work_type} in {property} in {self.path}.')
+
+    def raise_exceptions(self):
+        separator = '\n'
+        if len(self.all_exceptions) > 0:
+            raise Exception(f'{self.path} not valid. Has at least {len(self.all_exceptions)} errors including:\n\n{separator.join(self.all_exceptions)}')
+        else:
+            pass
 
 
 schema = json.load(open('schemas/schema.json'))
